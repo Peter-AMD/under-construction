84be76fd520796a96f43ff78a5c1cbf63ada39e1
Peter-AMD <gpeteamd@gmail.com>
2023-08-20 18:30:28 +0800

    UI : under construction page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,152 @@
-import Image from 'next/image'
+"use client";
+
+import * as THREE from "three";
+
+import React, { ComponentProps, use, useRef, useState } from "react";
+import { Canvas, useFrame, ThreeElements, extend } from "@react-three/fiber";
+import {
+  Text3D,
+  Center,
+  OrbitControls,
+  useGLTF,
+  Clone,
+  Environment,
+} from "@react-three/drei";
+
+const models = {
+  cauldron:
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/cauldron/model.gltf",
+  witch:
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/witch/model.gltf",
+  brokenWall:
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/broken-wall/model.gltf",
+  wall: "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/wall/model.gltf",
+  duck: "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/duck/model.gltf",
+  dog: "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/dog/model.gltf",
+  bear: "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/bear/model.gltf",
+  zombie:
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/zombie-1/model.gltf",
+};
+
+useGLTF.preload(models.wall);
+useGLTF.preload(models.brokenWall);
+useGLTF.preload(models.bear);
+useGLTF.preload(models.dog);
+useGLTF.preload(models.duck);
+useGLTF.preload(models.cauldron);
+useGLTF.preload(models.witch);
+useGLTF.preload(models.zombie);
+
+const Text = (props: ThreeElements["mesh"]) => {
+  return (
+    <Center top position={[0, 10, -12]}>
+      <Text3D font={"/chocolate_bar_regular.json"} {...props} scale={3}>
+        Soon to rise portfolio
+        <meshNormalMaterial />
+      </Text3D>
+    </Center>
+  );
+};
+
+type ModelProps = Omit<ComponentProps<typeof Clone>, "object"> & {
+  url: string;
+  object?: THREE.Object3D<THREE.Event> | THREE.Object3D<THREE.Event>[];
+};
+
+const Model = (props: ModelProps) => {
+  const { object, url, ...cProps } = props;
+  const { scene } = useGLTF(url);
+  return <Clone object={scene} {...cProps} />;
+};
+
+type SpecificModelProps = Omit<ModelProps, "url">;
+const DuckModel = (props: SpecificModelProps) => {
+  return <Model url={models.duck} {...props} />;
+};
+const DogModel = (props: SpecificModelProps) => {
+  return <Model url={models.dog} {...props} />;
+};
+const BearModel = (props: SpecificModelProps) => {
+  return <Model url={models.bear} {...props} />;
+};
+const ZombieModel = (props: SpecificModelProps) => {
+  return <Model url={models.zombie} {...props} />;
+};
+const WitchModel = (props: SpecificModelProps) => {
+  return <Model url={models.witch} {...props} />;
+};
+const CauldronModel = (props: SpecificModelProps) => {
+  return <Model url={models.cauldron} {...props} />;
+};
+const BrokenWallModel = (props: SpecificModelProps) => {
+  return <Model url={models.brokenWall} {...props} />;
+};
+const WallModel = (props: SpecificModelProps) => {
+  return <Model url={models.wall} {...props} />;
+};
+
+const WallGroup = () => {
+  return (
+    <group>
+      {/* Z wall */}
+      <WallModel position={[-16, 0, 1.31]} rotation={[0, 30, 0]} />
+      <WallModel position={[-15.4, 0, 5.27]} rotation={[0, 30, 0]} />
+      {/* End of Z wall */}
+
+      {/* X wall */}
+      <WallModel position={[-14, 0, 0]} />
+      <WallModel position={[-10, 0, 0]} />
+      <WallModel position={[-6, 0, 0]} />
+      <WallModel position={[-2, 0, 0]} />
+      <WallModel position={[2, 0, 0]} />
+      <BrokenWallModel position={[6, 0, 0]} />
+      <WallModel position={[10, 0, 0]} />
+      <WallModel position={[14, 0, 0]} />
+      {/* End of X wall */}
+
+      {/* Z wall */}
+      <WallModel position={[16, 0, 1.31]} rotation={[0, 30, 0]} />
+      <WallModel position={[16.6, 0, 5.27]} rotation={[0, 30, 0]} />
+      {/* End of Z wall */}
+    </group>
+  );
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex flex-col items-center justify-between h-[100vh]">
+      <div>
+        <p>Use your mouse buttons/scroll to move the scene!</p>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <Canvas camera={{ position: [-1, 6, 16], near: 0.025 }}>
+        <Environment
+          blur={0}
+          files={[
+            "/environment/px.png",
+            "/environment/nx.png",
+            "/environment/py.png",
+            "/environment/ny.png",
+            "/environment/pz.png",
+            "/environment/nz.png",
+          ]}
+          preset={undefined}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+        <Text />
+        <WallGroup />
+        <WitchModel position={[0, 0, 3]} scale={2} />
+        <CauldronModel position={[0, 0, 5.3]} scale={1.5} />
+        <DuckModel position={[-3, 0, 7]} rotation={[0, 2, 0]} />
+        <DogModel position={[-3, 0, 10]} rotation={[0, 2, 0]} />
+        <BearModel position={[3, 0, 7]} rotation={[0, -2, 0]} />
+        <ZombieModel position={[6, 0, -0.5]} rotation={[0, -0.5, 0]} />
+        <OrbitControls />
+      </Canvas>
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        Contact me on:{" "}
+        <a className="ml-3" href={`mailTo:gpeteamd@gmail.com`}>
+          gpeteamd@gmail.com
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </footer>
     </main>
-  )
+  );
 }
